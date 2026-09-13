22c6fa5ceaeacad292b802847d4101af90a88fe2
sagechang2020 <63951356+sagechang2020@users.noreply.github.com>
2026-09-13 02:49:16 +0100

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='pt-es-trip-2026-v1';
+const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','https://unpkg.com/leaflet@1.9.4/dist/leaflet.css','https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(CORE.map(u=>c.add(u))))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res;}).catch(()=>caches.match('./index.html'))));});
