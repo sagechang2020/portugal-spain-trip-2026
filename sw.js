2df46432641895fea2fe4e727421cf0bb848ab3c
sagechang2020 <63951356+sagechang2020@users.noreply.github.com>
2026-09-21 05:31:30 +0100

    Refresh cache after final Chinese traveler copy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pt-es-trip-2026-v10';
+const CACHE='pt-es-trip-2026-v11';
 const CORE=['./','./index.html','./home.css','./madeira.html','./styles.css','./app.js','./lisbon.html','./lisbon.css','./lisbon.js','./porto.html','./porto.css','./porto.js','./madrid.html','./madrid.css','./madrid.js','./manifest.webmanifest','./icon.svg','https://unpkg.com/leaflet@1.9.4/dist/leaflet.css','https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(CORE.map(u=>c.add(u)))))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
